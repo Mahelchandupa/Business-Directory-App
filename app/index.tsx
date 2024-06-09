0f64b1b98b592a913963f8d5f7821110dbda16f1
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-09 21:19:20 +0530

    implement tab navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Redirect href={'/home'} />
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -23,4 +23,5 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  PRIMARY: '#7F57F1'
 };
