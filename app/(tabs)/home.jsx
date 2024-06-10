ffb09356fa365fa60f329cffb8245ff50a89b14e
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-10 11:08:21 +0530

    complete home slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
+
 
 export default function home() {
   return (
@@ -8,7 +10,7 @@ export default function home() {
       {/* Header */}
       <Header />
       {/* Sldier */}
-
+      <Slider />
       {/* Category */}
 
       {/* Popular Business List */}
