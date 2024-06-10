e3be7155f47c1e6714d59743e1ded2df63be40ac
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-10 13:42:06 +0530

    complete home page popular business slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
 import Category from '../../components/Home/Category'
+import PopularBusiness from '../../components/Home/PopularBusiness'
 
 
 export default function home() {
   return (
-    <View>
+    <ScrollView>
       {/* Header */}
       <Header />
       {/* Sldier */}
@@ -15,6 +16,8 @@ export default function home() {
       {/* Category */}
       <Category />
       {/* Popular Business List */}
-    </View>
+      <PopularBusiness />
+      <View style={{ height: 50 }}></View>
+    </ScrollView>
   )
 }
---
+++ components/Home/PopularBusiness.jsx
@@ -0,0 +1,59 @@
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Colors } from '../../constants/Colors'
+import { collection, getDocs, limit, query } from 'firebase/firestore'
+import { db } from '../../configs/FirebaseConfig'
+import PopularBusinessCard from './PopularBusinessCard'
+
+export default function PopularBusiness() {
+
+  const [popularBusinessList, setPopularBusinessList] = useState([])
+
+  useEffect(() => {
+    getBusinessList()
+  }, [])
+
+  const getBusinessList = async () => {
+    setPopularBusinessList([])
+    const q = query(collection(db, "BusinessList"), limit(10))
+    const querySnapshot = await getDocs(q)
+
+    querySnapshot.forEach((doc) => {
+      setPopularBusinessList(prev => [...prev, doc.data()])
+    })
+  }
+
+  return (
+    <View>
+      <View
+        style={{
+          padding: 20,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: 'outfit-bold',
+          }}>
+          Popular Business
+        </Text>
+        <Text style={{ color: Colors.PRIMARY, fontFamily: 'outfit-medium' }}>View All</Text>
+      </View>
+      <FlatList
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        data={popularBusinessList}
+        renderItem={({ item, index }) => (
+          <PopularBusinessCard
+            business={item}
+            key={index}
+          />
+        )}
+      />
+    </View>
+  )
+}
