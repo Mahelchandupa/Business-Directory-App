10d6b8512f16e33fa23828d656cf149c264e4ee8
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-10 09:08:45 +0530

    create home page header section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
 
 export default function home() {
   return (
     <View>
-      <Text style={{ fontSize: 40, fontFamily: 'outfit' }}>home</Text>
+      {/* Header */}
+      <Header />
+      {/* Sldier */}
+
+      {/* Category */}
+
+      {/* Popular Business List */}
     </View>
   )
 }
