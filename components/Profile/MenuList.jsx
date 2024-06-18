80778d7f8ebe2583f84b215f563c614737305a88
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-18 10:51:34 +0530

    design and implement add new business screen

--- components/Profile/MenuList.jsx
+++ components/Profile/MenuList.jsx
@@ -1,15 +1,18 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Colors } from './../../constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function MenuList() {
+
+    const router = useRouter()
   
   const menuList = [
     {
         id: 1,
         name: 'Add Business',
         icon: require('./../../assets/images/add.png'),
-        path: ''
+        path: '/business/add-business'
     },
     {
         id: 2,
@@ -30,6 +33,10 @@ export default function MenuList() {
         path: ''
     }
   ]  
+
+  const onMenuClick = (item) => {
+    router.push(item.path)
+  }
     
   return (
     <View style={{ marginTop: 50 }}>
@@ -37,7 +44,7 @@ export default function MenuList() {
         data={menuList}
         numColumns={2}
         renderItem={({ item }) => (
-          <View style={{
+          <TouchableOpacity onPress={() => onMenuClick(item)} style={{
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
@@ -55,7 +62,7 @@ export default function MenuList() {
                 height: 40
              }} />
              <Text style={{ fontFamily: 'outfit-medium', fontSize: 14, flex: 1 }}>{item.name}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>
