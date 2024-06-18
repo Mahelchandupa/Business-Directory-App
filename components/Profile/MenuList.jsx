386a8781dbd7fc050aade3ebdd5b1a14f5d01f6f
Mahel Chandupa <mahelchandupa1013@gmail.com>
2024-06-18 11:57:12 +0530

    implement logout and share functionalty

--- components/Profile/MenuList.jsx
+++ components/Profile/MenuList.jsx
@@ -1,11 +1,13 @@
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, Share } from 'react-native'
 import React from 'react'
 import { Colors } from './../../constants/Colors'
 import { useRouter } from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
 
 export default function MenuList() {
 
     const router = useRouter()
+    const { signOut } = useAuth()
   
   const menuList = [
     {
@@ -24,17 +26,28 @@ export default function MenuList() {
         id: 3,
         name: 'Share App',
         icon: require('./../../assets/images/share_1.png'),
-        path: ''
+        path: 'share'
     },
     {
         id: 4,
         name: 'Logout',
         icon: require('./../../assets/images/logout.png'),
-        path: ''
+        path: 'logout'
     }
   ]  
 
   const onMenuClick = (item) => {
+    if (item.path === 'logout') {
+      signOut()
+      return;
+    }
+
+    if (item.path === 'share') {
+      Share.share({
+        message: 'Download the business directory app'
+      })
+      return;
+    }
     router.push(item.path)
   }
     
